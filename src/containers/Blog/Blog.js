9e9639f3742e817ce311a3312a8113625af1339c
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-11-29 15:00:07 -0800

    Set up rendering routes, then render components for Routes, and switch between Pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
-
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -18,7 +19,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home 2</h1>}/> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
